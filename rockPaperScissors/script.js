d4d3baa4d112bcd26e236ca285108a6ad378cc1d
Zoe Carmona <zocaya2016@gmail.com>
2025-06-14 19:10:42 -0600

    Functional button

---
+++ rockPaperScissors/script.js
@@ -0,0 +1,6 @@
+// Playing the game
+document.getElementById("playButton").addEventListener("click", function(event) {
+    window.location.href = "rockPaperScissors.html";
+});
+
+// Returning to the main menu
